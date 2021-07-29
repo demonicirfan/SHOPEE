c5da7e0421ceaa23444e30f6f22edb8f37d84565
demonicirfan <hashtag.irfan@gmail.com>
2021-07-29 18:41:00 +0530

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProductPage);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProductPage);
+
+module.exports = router;
+
+
