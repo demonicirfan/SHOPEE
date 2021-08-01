25c1f4ac98b1d61393ea780b3819b2d736c6f6a0
demonicirfan <hashtag.irfan@gmail.com>
2021-08-01 15:52:17 +0530

    code working

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Tacmp@508'
+});
+
+module.exports = pool.promise();
